a7bb2f6bbba05e321fcc984049a896b2f12081e2
Saif Eddine Jerbi <sejerbi@vermeg.com>
2019-07-21 00:20:41 +0300

    update systemjs

--- platform/src/client/app/core/module-container/services/system-js-module-loader.service.ts
+++ platform/src/client/app/core/module-container/services/system-js-module-loader.service.ts
@@ -7,6 +7,9 @@ import * as angularCommonHttp from '@angular/common/http';
 import * as angularCommon from '@angular/common';
 import * as angularPlatformBrowser from '@angular/platform-browser';
 import * as angularPlatformBrowserDynamic from '@angular/platform-browser-dynamic';
+import * as sharedDataApiClient from 'odessajs19-plg-platform-shared-api/client/data.api';
+import * as rxjsCore from 'rxjs';
+import * as rxjsOperatorCore from 'rxjs/operators';
 
 import { Observable, from } from 'rxjs';
 
@@ -26,6 +29,13 @@ SystemJS.set('@angular/core', SystemJS.newModule(angularCore));
 SystemJS.set('@angular/forms', SystemJS.newModule(angularForms));
 SystemJS.set('@angular/common/http', SystemJS.newModule(angularCommonHttp));
 SystemJS.set('@angular/common', SystemJS.newModule(angularCommon));
+SystemJS.set('rxjs', SystemJS.newModule(rxjsCore));
+SystemJS.set('rxjs/operators', SystemJS.newModule(rxjsOperatorCore));
+
+SystemJS.set(
+  'odessajs19-plg-platform-shared-api/client/data.api',
+  SystemJS.newModule(sharedDataApiClient)
+);
 
 @Injectable()
 export class CoreSystemJsModuleLoader {
