8d3fc70ebfbc127f03b457ef8eed367e03ada165
Saif Eddine Jerbi <sejerbi@vermeg.com>
2019-07-20 22:20:15 +0300

    update module

--- plugin/src/client/app/app.module.ts
+++ plugin/src/client/app/app.module.ts
@@ -2,17 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MyPluginComponent } from './my-plugin/my-plugin.component';
+import { MyPluginModule } from './my-plugin/public-api';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MyPluginComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, MyPluginModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
